8feee8ac0a01ef7c03a2e98e957996c286970fd5
eswther <3263428923@qq.com>
2025-10-08 11:13:36 +0800

    完善注册功能，添加相关mapper接口的数据库操作

--- src/main/java/com/management/laboratory/controller/UserController.java
+++ src/main/java/com/management/laboratory/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.management.laboratory.controller;
 import com.management.laboratory.entity.User;
+import com.management.laboratory.mapper.AdminMapper;
+import com.management.laboratory.mapper.StudentMapper;
+import com.management.laboratory.mapper.TeacherMapper;
 import com.management.laboratory.mapper.UserMapper;
 import com.management.laboratory.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,12 @@ public class UserController {
 
     @Autowired
     UserMapper userMapper;
+    @Autowired
+    AdminMapper adminMapper;
+    @Autowired
+    StudentMapper studentMapper;
+    @Autowired
+    TeacherMapper teacherMapper;
     @Autowired
     UserService userService;
 
@@ -35,10 +44,28 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public boolean login(String username, String password) {
-        // Implement login logic here
+    public boolean login(@RequestBody Map<String, String> userInfo) {
+        User loginUser = userMapper.selectUserByAccount(userInfo.get("account"));
+        if (loginUser == null){
+            return false; // Account does not exist
+        }else if (!loginUser.getPassword().equals(userInfo.get("password"))){
+            return false;
+        }else {
+            userService.setShareUser(loginUser);
+            switch (loginUser.getPermission()){
+                case 0:
+                    userService.setShareAdmin(adminMapper.selectAdminByUserId(loginUser.getUserId()));
+                    break;
+                case 1:
+                    userService.setShareTeacher(teacherMapper.selectTeacherByUserId(loginUser.getUserId()));
+                    break;
+                case 2:
+                    userService.setShareStudent(studentMapper.selectStudentByUserId(loginUser.getUserId()));
+                    break;
+                default:
+                    return false;
+            }
+        }
         return true; // Placeholder return value
     }
-
-
 }
